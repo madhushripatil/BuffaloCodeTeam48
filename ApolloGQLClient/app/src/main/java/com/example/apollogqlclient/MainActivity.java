673cd13955a2806a08cfa0181cee8ed36be39979
Madhushri Patil <madhushripatil@gmail.com>
2019-06-15 14:16:51 -0400

    Madhushri - Andriod project client GUI

---
+++ ApolloGQLClient/app/src/main/java/com/example/apollogqlclient/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.apollogqlclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.activityTypes));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+    }
+}
